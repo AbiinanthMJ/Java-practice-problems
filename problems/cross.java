bab981d4c53e5c7ca0ab7304aef536d5518c738c
abiinanthmjashvanthmagmail.com <abiinanthmjashvanthm@gmail.com>
2025-03-08 03:09:33 +0530

    "For those think i am not capable of doing this F**....yourself"

---
+++ problems/cross.java
@@ -0,0 +1,30 @@
+package problems;
+
+public class cross {
+    public static void main(String[] args) {
+                      int count=0;
+                      int size=6;
+                  int arm2count=1;
+                 int arm1count=1;
+                for (int i = 0; i <=6; i++) {
+                    for (int j = 0; j <=6; j++) {
+                        if(i==j && i<=6/2){
+                            System.out.print(arm1count++);
+                        } else if (i==j && i>6/2) {
+                            System.out.print(--arm1count-1);
+                        } else if (i ==count&& j==size) {
+                            System.out.print(arm2count++);
+                            count++;
+                            size--;
+//                           3,3
+                        } else if (i==count+1 && j==size-1) {
+                            System.out.print(--arm2count);
+                            count++;
+                            size--;
+                        }
+                        System.out.print(" "+"\t");
+                    }
+                    System.out.println();
+                }
+    }
+}
