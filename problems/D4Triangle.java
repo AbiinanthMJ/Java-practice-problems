d7773163eb798440c0ec500b1d04c09eb6a2236a
Abiinanth.m.j <abiinanthmjashvanthm@gmail.com>
2025-05-19 19:00:42 +0530

    "fuck yourself"

---
+++ problems/D4Triangle.java
@@ -0,0 +1,21 @@
+package problems;
+import java.util.*;
+public class D4Triangle {
+        public String triangleType(int[] nums) {
+            Arrays.sort(nums);
+            int a = nums[0];
+            int b = nums[1];
+            int c= nums[2];
+            if (a + b <= c)return "none";
+            if(a==b&&b==c)return "equilateral";
+            if(a==b||b==c||c==a)return "isosceles";
+            else return "scalene";
+        }
+
+    public static void main(String[] args) {
+        D4Triangle tc = new D4Triangle();
+        String s=tc.triangleType(new int[]{3,1,3});
+        System.out.println(s);
+    }
+    }
+
