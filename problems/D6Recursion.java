2b2802070e5beccf6a1393be87ccd08f81a1da02
Abiinanth.m.j <abiinanthmjashvanthm@gmail.com>
2025-05-21 23:58:23 +0530

    "fuck yourself"

---
+++ problems/D6Recursion.java
@@ -0,0 +1,53 @@
+package problems;
+
+import java.util.Scanner;
+
+public class D6Recursion {
+    void fun(int i , int n ){
+        if(i>n){
+            return;
+        }
+        System.out.println("AS");
+        fun(i+1,n);
+    }
+    void fun2(int i){
+        if(i>5){
+            return;
+        }
+        System.out.println(i);
+        fun2(i+1);
+    }
+    void fun3(int i , int n){
+        if(n<1){
+            return;
+        }
+        System.out.println(n);
+        fun3(1,n-1);
+    }
+    void backtracking(int n1,int n2){
+        if(n1<0){
+            return;
+        }
+        backtracking(n1-1,n2);
+        System.out.println(n1);
+    }
+    void backtracking2(int n1,int n2){
+          if(n2+n2<n1){
+              return;
+          }
+         backtracking2(n1+1,n2);
+         System.out.println(n1-n2);
+    }
+//    We did it Boi
+    public static void main(String[] args) {
+        D6Recursion obj = new D6Recursion();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+//        obj.fun(1,n);
+//        obj.fun2(1);
+//        obj.fun3(1,n);
+//          obj.backtracking(n,n);
+        obj.backtracking2(n, n);
+
+    }
+}
